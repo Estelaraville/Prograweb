34ca421e1e1deb6062a1318201155bc69d87e1e8
Estelaraville <elarav2200@alumno.ipn.mx>
2024-10-22 17:43:48 -0600

    Clase 22 de octubre

---
+++ clase 22-10-2024/script.js
@@ -0,0 +1,20 @@
+//01 FUNCIÓN NORMAL
+function hola(a,b){
+    return a+b;
+}
+
+console.log(hola(3,5));
+
+//02 CONVERIT A FUNCIÓN FLECHA
+const suma = (a,b) => {
+    return a+b;
+}
+console.log(suma(3,5));
+
+//03 Simplificar
+const suma2 = (a,b) => a+b;
+console.log(suma2(5,5));
+
+//04 solo un parámetro
+const cuadrado = x => x*x;
+console.log(cuadrado(5));
