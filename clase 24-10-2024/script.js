d262869150f22d99d80f0ff6a324aa95f588f043
Estelaraville <elarav2200@alumno.ipn.mx>
2024-10-24 16:42:13 -0600

    Arrays

---
+++ clase 24-10-2024/script.js
@@ -0,0 +1,26 @@
+//Arreglos
+
+/*
+let array = ["dato 1", 1, Boolean];
+console.log("tamaño original:"+array.length);
+array.push("5");
+console.log(array[3]);
+console.log("nuevo tamaño:"+array.length);
+*/
+
+
+
+/*
+Hacer la tabla del 5
+*/
+
+
+let array2 =[];
+for(i=0; i<10; i++){
+    if(array2.length <6){
+        array2.push(5*i)
+        console.log(array2)
+    }
+    
+}
+console.log("El array tiene "+array2.length) 
